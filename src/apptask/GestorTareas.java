83edee09af6c1d88f7dad4e31d64db82d3f01d50
felixsolis <felixsolis@localhost>
2026-07-14 21:50:28 -0600

    Agregada clase GestorTareas con Colecciones y Excepciones

---
+++ src/apptask/GestorTareas.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class GestorTareas {
+    
+    private ArrayList<TareaBase> listaTareas;
+
+    public GestorTareas() {
+        this.listaTareas = new ArrayList<>();
+    }
+
+
+    public void agregarTarea(TareaBase nuevaTarea) throws Exception {
+        if (nuevaTarea == null) {
+            throw new Exception("Error fatal: Se intentó guardar una tarea vacía.");
+        }
+        listaTareas.add(nuevaTarea);
+    }
+    
+    public ArrayList<TareaBase> obtenerTodasLasTareas() {
+        return listaTareas;
+    }
+    
+    public void completarTarea(int indice) throws Exception {
+        if (indice < 0 || indice >= listaTareas.size()) {
+            throw new Exception("Error: La tarea seleccionada no existe en el sistema.");
+        }
+        listaTareas.get(indice).marcarComoCompletada();
+    }
+}
